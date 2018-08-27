cab5026e222abe21cd5db19b31b5b9317a6c7227
unknown <w00jt3k.88@gmail.com>
2018-08-27 18:18:18 +0200

    From laptop

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,8 @@
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 var template = React.createElement(
@@ -13,10 +14,15 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
         'ol',
@@ -39,9 +45,17 @@ var user = {
     age: 30,
     location: 'Białystok'
 };
-var userName = 'Siemka eniu';
-var userAge = 26;
-var userLocation = 'Ta';
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
 
 var templateTwo = React.createElement(
     'div',
@@ -49,20 +63,15 @@ var templateTwo = React.createElement(
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 var template = (
     <div>
         <h1>{ app.title }</h1>
-        <p>{ app.subtitle }</p>
+        { app.subtitle && <p>{ app.subtitle }</p> }
+        <p>{ app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -20,15 +22,18 @@ var user = {
     age: 30,
     location: 'Białystok'
 };
-var userName = 'Siemka eniu';
-var userAge = 26;
-var userLocation = 'Ta';
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: { location }</p>;
+    }
+}
 
 var templateTwo = (
     <div>
-        <h1>{ user.name }</h1>
-        <p>Age: { user.age }</p>
-        <p>Location: { user.location }</p>
+        <h1>{ user.name ? user.name : 'Anonymous' }</h1>
+        { (user.age && user.age >= 18) && <p>Age: { user.age }</p> }
+        { getLocation(user.location) }
     </div>
 );
 
