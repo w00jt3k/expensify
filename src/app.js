4e682436c85b34d6695c9dc3ee8a65ec4b75960e
w00jt3k <w00jt3k.88@gmail.com>
2018-09-02 17:13:39 +0200

    Niedziela

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-
-var app = {
+const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
     options: ['One', 'Two']
 };
 
-var template = (
+const template = (
     <div>
         <h1>{ app.title }</h1>
         { app.subtitle && <p>{ app.subtitle }</p> }
@@ -17,19 +16,19 @@ var template = (
     </div>
 );
 
-var user = {
+const user = {
     name: 'Wojtek',
     age: 30,
     location: 'Białystok'
 };
 
-function getLocation(location) {
+const getLocation(location) {
     if (location) {
         return <p>Location: { location }</p>;
     }
 }
 
-var templateTwo = (
+const templateTwo = (
     <div>
         <h1>{ user.name ? user.name : 'Anonymous' }</h1>
         { (user.age && user.age >= 18) && <p>Age: { user.age }</p> }
@@ -37,6 +36,6 @@ var templateTwo = (
     </div>
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,16 @@
+var nameVar = 'Andrew';
+var nameVar = 'Mike';
+console.log('nameVar', nameVar);
+
+let nameLet = 'Julie';
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+console.log('nameConst', nameConst);
+
+function getPetName() {
+    const petName = 'Hal';
+    return petName;
+}
+
+console.log(petName);
