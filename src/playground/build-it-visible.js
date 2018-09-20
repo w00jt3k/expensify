3df3b27849dd3d3aeca3d5e1595a7401b117de80
w00jt3k <w00jt3k.88@gmail.com>
2018-09-20 21:04:32 +0200

    Added state

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,22 +1,31 @@
-let isVisible = false;
-const appRoot = document.getElementById('app');
+class VisbilitToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        };
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
+                {this.state.visibility && (
+                    <div>
+                        <p>Hey. These are some details you can now see!</p>
+                    </div>
+                )}             
+            </div>
+        );
+    }
+}
 
-const onButtonClick = () => {
-    isVisible = !isVisible;
-
-    render();
-};
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visiblilty Toggle</h1>
-            <button onClick={ onButtonClick }>{ isVisible ? 'Hide Details' : 'Show Details' }</button>
-            { isVisible && <p>Some text lorem ipsum :)</p> }
-        </div>
-    );
-    
-    ReactDOM.render(template, appRoot);
-};
-
-render();
+ReactDOM.render(<VisbilitToggle />, document.getElementById('app'));
