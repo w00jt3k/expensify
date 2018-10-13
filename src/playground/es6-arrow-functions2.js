bc8f9d122bcf5f70f00f51a8fb7634dff6b5edc3
w00jt3k <w00jt3k.88@gmail.com>
2018-10-13 21:30:31 +0200

    Init

--- src/playground/es6-arrow-functions2.js
+++
@@ -1,9 +0,0 @@
-const multiplier = {
-    numbers: [1, 2, 3],
-    multiplyBy: 2,
-    multiply() {
-        return this.numbers.map((number) => number * this.multiplyBy);
-    }
-};
-
-console.log(multiplier.multiply());
